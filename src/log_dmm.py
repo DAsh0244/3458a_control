a54e31205e69dc8c45cded3a2a9f11ee506c08f4
Danyal <an.oreo@live.com>
2019-02-27 11:15:51 -0600

    added timeout for log_dmm.py

--- src/log_dmm.py
+++ src/log_dmm.py
@@ -1,6 +1,9 @@
 from control import HP3458a
 from datetime import datetime
 import sys
+import time
+# run time is ~ 3.5 -> 3.7 Hrs = 3.7*3600 s
+RUN_TIME = 3.7*3600
 
 if __name__ == "__main__":
     dmm = HP3458a(port=sys.argv[1],baud=9600)
@@ -10,12 +13,17 @@ if __name__ == "__main__":
     dmm.send_cmd('NRDGS 1 TIMER')
     dmm.send_cmd('TARM AUTO')
     dmm.send_cmd('TRIGGER AUTO')
+    timeout = False
     try:
-        with open('log_{}.txt'.format(str(datetime.now()).replace(' ','_').replace(':','_')),'w') as outfile:
-            while True:
+        with open('../logs/log_{}.txt'.format(str(datetime.now()).replace(' ','_').replace(':','_')),'w') as outfile:
+            print('logging')
+            start_time = time.time()
+            while not timeout:
                 vout = dmm.read_response()
                 ts = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-                print('{},{}\n'.format(ts,vout))
+                # print('{},{}\n'.format(ts,vout))
                 outfile.write('{},{}\n'.format(ts,vout))
+                timeout = (time.time() - start_time) > RUN_TIME
     except KeyboardInterrupt:
         pass
+    print('done')
