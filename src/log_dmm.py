5e5a8d052a2cdff99fe3ed200ba6b9d9b639f5c9
an-oreo <an.oreo@live.com>
2019-02-26 20:26:02 -0600

    fixed loging script

--- src/log_dmm.py
+++ src/log_dmm.py
@@ -1,22 +1,20 @@
 from control import HP3458a
+from datetime import datetime
 import sys
-import time
-import datetime
-
-PORT = 'COM3'
 
 if __name__ == "__main__":
-    dmm = HP3458a(port=PORT)
+    dmm = HP3458a(port=sys.argv[1],baud=9600)
     dmm.stop_readings()
-    dmm.clear_buffer()
     dmm.set_digits(9)
     dmm.set_sample_rate(1)
     dmm.send_cmd('NRDGS 1 TIMER')
-    try: 
-        with open('log.txt','w') as outfile:
+    dmm.send_cmd('TARM AUTO')
+    dmm.send_cmd('TRIGGER AUTO')
+    try:
+        with open('log_{}.txt'.format(str(datetime.now()).replace(' ','_').replace(':','_')),'w') as outfile:
             while True:
-                data = dmm.read_response()
-                print(data)
-                outfile.write(data+'\n')
+                vout = dmm.read_response()
+                print(vout)
+                outfile.write('{},{}\n'.format(str(datetime.now()),vout))
     except KeyboardInterrupt:
         pass
